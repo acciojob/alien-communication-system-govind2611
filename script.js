893cc781e39112d88de6c4509c40a13edc3dd8c8
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-11-17 09:19:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+
+const sendMicrotask = (message) => {
+  console.log(`Processing microtask: ${message}`);
+};
+
+const sendMacrotask = (message) => {
+  console.log(`Processing macrotask: ${message}`);
+};
+
+const aliensCommunication = () => {
+  console.log("Aliens are communicating!");
+
+  Promise.resolve()
+    .then(() => sendMicrotask("Microtask 1"));
+
+  Promise.resolve()
+    .then(() => sendMicrotask("Microtask 2"));
+
+
+  setTimeout(() => sendMacrotask("Macrotask 1"), 0);
+
+ 
+  setTimeout(() => sendMacrotask("Macrotask 2"), 0);
+
+  console.log("Communication tasks sent!");
+};
+
+aliensCommunication();
+
+console.log("Earth is processing tasks...");
